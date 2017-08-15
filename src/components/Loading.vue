387134ff07c30b346541323bbc536e061cfdccdc
Joey Frohikey <frohikey@gmail.com>
2017-08-15 19:28:20 +0200

    Fix jumping-square

--- src/components/Loading.vue
+++ src/components/Loading.vue
@@ -1,28 +1,30 @@
-<template>    
-   <div :id="id" :class="['nk-loading', active ? 'loading' : '', fullscreen ? 'fullscreen' : '']">
-          
-      <div class="spinner" :style="sizeStyle">
-        <div v-if="type == 'double-bounce'" class="double-bounce1" :style="loadingStyle"></div>
-        <div v-if="type == 'double-bounce'" class="double-bounce2" :style="loadingStyle"></div>
-      </div>
+<template>
+    <div :id="id" :class="['nk-loading', active ? 'loading' : '', fullscreen ? 'fullscreen' : '']">
+    
+        <div v-if="type == 'double-bounce'" class="spinner double-bounce" :style="sizeStyle">    
+            <div :style="loadingStyle"></div>
+            <div :style="loadingStyle"></div>                         
+        </div>
+    
+        <div v-if="type == 'jumping-square'" class="spinner jumping-square" :style="sizeStyle"></div>
 
-      <slot></slot>
-
-    </div>    
+        <slot></slot>
+    
+    </div>
 </template>
 <script>
-  export default {
+export default {
     props: {
         id: {
             type: String,
             default: function () {
                 return 'loading-id-' + this._uid;
             },
-        },            
+        },
         type: {
             type: String,
             default: 'double-bounce'
-        },     
+        },
         fillColor: {
             type: String,
             default: '#000000'
@@ -41,30 +43,30 @@
             type: Boolean,
             default: false
         }
-    },    
-    computed: {        
+    },
+    computed: {
         loadingStyle() {
             if (this.type == 'double-bounce') {
                 return {
                     backgroundColor: this.fillColor,
                     animationDuration: this.duration
-                }   
-            }            
+                }
+            }
         },
         sizeStyle() {
             return {
                 width: this.size,
-                height: this.size                
-            }    
-        }        
-    }    
-  }
+                height: this.size
+            }
+        }
+    }
+}
 </script>
 <style lang="scss" scoped>
-
+// https://github.com/nguyenvanduocit/vue-loading-spinner
 .nk-loading {
     position: relative;
-    
+
     &.loading {
         &:before {
             content: '';
@@ -91,33 +93,64 @@
             }
         }
     }
-    
-    .spinner {        
-        position: absolute; 
+
+    .spinner {
+        position: absolute;
         left: 50%;
-        top: 50%; 
-        transform: translate(-50%, -50%); 
+        top: 50%;
+        transform: translate(-50%, -50%);
         display: none;
         z-index: 1000;
 
         * {
             line-height: 0;
             box-sizing: border-box;
-        }
+        } 
+        
+        // *** DOUBLE-BOUNCE ***
+        &.double-bounce {
+            &>div {        
+                width: 100%;
+                height: 100%;
+                border-radius: 50%;
+                opacity: 0.6;
+                position: absolute;
+                top: 0;
+                left: 0;
+                animation: double-bounce 2.0s ease-in-out infinite;
 
-        .double-bounce1,
-        .double-bounce2 {
-            width: 100%;
-            height: 100%;
-            border-radius: 50%;
-            opacity: 0.6;
-            position: absolute;
-            top: 0;
-            left: 0;
-            animation: double-bounce 2.0s ease-in-out infinite;
+                &:nth-child(2) {
+                    animation-delay: -1.0s;
+                }
+            }
         }
-        .double-bounce2 {
-            animation-delay: -1.0s;
+        
+        // *** JUMPING-SQUARE ***        
+        &.jumping-square {
+            &:before {
+                content: '';
+                width: 100%;
+                height: 20%;
+                min-width: 5px;
+                background: #000;
+                opacity: 0.1;
+                position: absolute;
+                bottom: 0%;
+                left: 0;
+                border-radius: 50%;
+                animation: rotate-square-2-shadow .5s linear infinite;
+            }
+            &:after {
+                content: '';
+                width: 100%;
+                height: 100%;
+                background: #000;
+                animation: rotate-square-2-animate .5s linear infinite;
+                position: absolute;
+                bottom: 40%;
+                left: 0;
+                border-radius: 3px;
+            }
         }
     }
 }
@@ -132,5 +165,32 @@
     }
 }
 
+@keyframes rotate-square-2-animate {
+    17% {
+        border-bottom-right-radius: 3px;
+    }
+    25% {
+        transform: translateY(20%) rotate(22.5deg);
+    }
+    50% {
+        transform: translateY(40%) scale(1, .9) rotate(45deg);
+        border-bottom-right-radius: 50%;
+    }
+    75% {
+        transform: translateY(20%) rotate(67.5deg);
+    }
+    100% {
+        transform: translateY(0) rotate(90deg);
+    }
+}
 
+@keyframes rotate-square-2-shadow {
+    0%,
+    100% {
+        transform: scale(1, 1);
+    }
+    50% {
+        transform: scale(1.2, 1);
+    }
+}
 </style>
